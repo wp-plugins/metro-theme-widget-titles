435b59c3f62952208bded14cf0fe70e7b3c054b4
bhwebworks <bhwebworks@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-09-09 20:39:43 +0000

    Updating trunk to version 0.2
    git-svn-id: https://plugins.svn.wordpress.org/metro-theme-widget-titles/trunk@769366 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- metro-theme-widget-titles.php
+++ metro-theme-widget-titles.php
@@ -3,8 +3,8 @@
 /**
  * Plugin Name: Metro Theme Widget Titles
  * Plugin URI: http://blackhillswebworks.com
- * Description: Fixes a theme/plugin compatibility issue by removing the StudioPress Metro theme widget title filter and adding another filter that is compatible with plugins.
- * Version: 0.1
+ * Description: Fixes a theme/plugin compatibility issue by removing the StudioPress Metro theme widget title filter and adding another filter that is compatible with plugins. Version 0.2 restores the 'old' Metro look to Metro Pro widget titles, comments title and comments reply title.
+ * Version: 0.2
  * Author: John Sundberg
  * Author URI: http://blackhillswebworks.com
  * License: GPLv2 or later
@@ -33,6 +33,7 @@
 /**
  * Remove the Metro theme widget title filter
  *
+ * @since 0.1
  */
 
 	add_action( 'genesis_before', 'remove_metro_widget_title_filter' );
@@ -42,12 +43,13 @@
 		remove_filter( 'widget_title', 'metro_widget_title' );
 
 	}
-	
+
 
 
 /**
  * Now re-add the span class to widget titles in a way that is compatible with plugins
  *
+ * @since 0.1
  */	
 
 	add_filter( 'genesis_register_sidebar_defaults', 'bhww_metro_widget_titles' );
@@ -57,8 +59,127 @@
 		return 	array(
 			'before_widget' => '<div id="%1$s" class="widget %2$s"><div class="widget-wrap">',
 			'after_widget'  => "</div></div>\n",
-			'before_title'  => '<h4 class="widgettitle"><span class="widget-headline">', // Added span tag and class
+			'before_title'  => '<h4 class="widgettitle widget-title"><span class="widget-headline">', // Added span tag and class
 			'after_title'   => "</span></h4>\n", // Added closing span tag
 		);
+		
+	}
+
+
+	
+/**
+ * Check to see if Metro (original, not Pro) is installed AND that HTML5 is enabled. 
+ * If so, restore the comment reply title to the original Metro theme look.
+ *
+ * @since 0.2
+ */	
+	
+	add_action( 'genesis_init', 'bhww_check_for_metro_theme' );
+	
+	function bhww_check_for_metro_theme() {
+		
+		$theme = wp_get_theme( 'metro' );
+		
+		if ( $theme->exists() ) {
+
+			add_action( 'genesis_before', 'bhww_metro_filter_genesis_html5_reply_title' );
+
+			function bhww_metro_filter_genesis_html5_reply_title() {
+			
+				// If this is not inside a function called with an action, things break!
+				if ( genesis_html5() ) {
+				
+					// This filter is found in wp-includes/comment-template.php (at line 1641 in version 3.6)
+					add_filter( 'comment_form_defaults', 'bhww_metro_title_reply_filter' );
+					
+					function bhww_metro_title_reply_filter ($arg) {
+					
+						$arg['title_reply'] = '<span class="comments-title">' . __( 'Leave a Reply' ) . '</span>';
+						return $arg;
+					
+					}
+					
+				}
+				
+			}
+				
+		}
+		
+	}
+	
+
+
+/**
+ * Check to see if Metro Pro is installed, and if it is, restore the widget titles,
+ * comments title, and comment reply title to the original Metro theme look
+ *
+ * @since 0.2
+ */	
+	
+	add_action( 'genesis_init', 'bhww_check_for_metro_pro_theme' );
+	
+	function bhww_check_for_metro_pro_theme() {
+		
+		$theme = wp_get_theme( 'metro-pro' );
+		
+		if ( $theme->exists() ) {
+				
+			/**
+			 * Add the 'comments header text in comments' filter that was removed in Metro Pro
+			 *
+			 * @since 0.2
+			 */
+				
+				add_filter( 'genesis_title_comments', 'bhww_metro_pro_title_comments' );
+				
+				function bhww_metro_pro_title_comments() {
+				
+					$title = '<h3><span class="comments-title">' . __( 'Comments', 'metro' ) . '</span></h3>';
+					return $title;
+					
+				}
+				
+				
+
+			/**
+			 * Add a span class to the 'Leave a Reply' text
+			 *
+			 * @since 0.2
+			 */
+
+				add_action( 'genesis_before', 'bhww_metro_pro_filter_genesis_html5_reply_title' );
+
+				function bhww_metro_pro_filter_genesis_html5_reply_title() {
+					
+					// This filter is found in wp-includes/comment-template.php (at line 1641 in version 3.6)
+					add_filter( 'comment_form_defaults', 'bhww_metro_pro_title_reply_filter' );
+					
+					function bhww_metro_pro_title_reply_filter ($arg) {
+					
+						$arg['title_reply'] = '<span class="comments-title">' . __( 'Leave a Reply' ) . '</span>';
+						return $arg;
+					
+					}
+					
+				}
+
+
+
+			/**
+			 * Register and enqueue the plugin stylesheet
+			 *
+			 * @since 0.2
+			 */
+			 
+				add_action( 'wp_enqueue_scripts', 'bhww_metro_pro_widget_titles_styles' );
+				
+				function bhww_metro_pro_widget_titles_styles() {
+					
+					wp_register_style( 'bhww-mtwt', plugins_url( 'css/metro-theme-widget-titles-style.css', __FILE__ ), array(), '20130907a' );
+					wp_enqueue_style( 'bhww-mtwt' );
+					
+				}
+				
+		}
 		
 	}
--- readme.txt
+++ readme.txt
@@ -3,8 +3,8 @@ Contributors: bhwebworks
 Donate link: http://blackhillswebworks.com/donate
 Tags: StudioPress Metro theme, widget titles
 Requires at least: 3.3
-Tested up to: 3.5.1
-Stable tag: 0.1
+Tested up to: 3.6
+Stable tag: 0.2
 License: GPLv2 or later
 
 Out of the box, StudioPress Metro theme widget titles are not compatible with some plugins. This plugin fixes that.
@@ -14,6 +14,12 @@ Out of the box, StudioPress Metro theme widget titles are not compatible with so
 The StudioPress Metro theme adds a span tag and class to widget titles using a filter in the theme's functions.php file, and then styles that class with CSS. Unfortunately this method is not compatible with plugins that escape html in widget titles. 
 
 Metro Theme Widget Titles removes the default Metro widget title filter and replaces it with a filter that is compatible with plugins.
+
+**Added in version 0.2**
+
+If you've updated the original Metro theme to HTML5 this plugin will restore the look of the "Leave a Reply" title in the comment form. It may also undo any change you've made to that text. If it does, you can edit the text in the plugin's filter, live with the change, or deactivate the plugin.
+
+If you are using the Metro Pro theme, this plugin will restore the look of your widget titles, comments title, and "Leave a Reply" title to the old Metro theme look.
 
 **This plugin is intended for websites using the Genesis framework, and specifically the StudioPress Metro theme**
 
@@ -33,7 +39,7 @@ No, it won't. This plugin references hooks that are specific to Genesis. If you 
 
 = No settings page? =
 
-Nope. This plugin is really simple and does one thing.
+Nope. This plugin is really simple and does one thing. Well, maybe a couple things now...
 
 == Screenshots ==
 
@@ -42,5 +48,10 @@ Nope. This plugin is really simple and does one thing.
 
 == Changelog ==
 
+= 0.2 =
+* Confirm compatibility with WordPress 3.6 and Genesis 2.0
+* Restore Metro Pro widget titles to the original Metro style
+* Removed screenshots from trunk (they're still in the assets directory)
+
 = 0.1 =
-Initial release
+* Initial release
